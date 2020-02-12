2823aaa25af9aa5c6bc8093fd4b2969f92cc9189
Titu <email.to.titu@gmail.com>
2020-02-13 01:03:41 +0600

    fix on link and config

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,7 +2,7 @@
   <div>
     <ul>
       <li v-for="blog in blogPosts" :key="blog.slug">
-        <router-link :to="blog.slug">{{blog.title}}</router-link>
+        <router-link :to="`/blog/${blog.slug}`">{{blog.title}}</router-link>
       </li>
     </ul>
     <router-view></router-view>
